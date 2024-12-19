b88a2943134bc47dc5bf77eea4145af9779ef0a0
Rohan <rohanagrawal8989@gmail.com>
2024-12-19 16:30:00 +0530

    Removed console statements

--- src/hooks/useTopRatedMovies.js
+++ src/hooks/useTopRatedMovies.js
@@ -7,8 +7,6 @@ const useTopRatedMovies = () => {
   const dispatch = useDispatch();
 
   const topRatedMovies = async () => {
-    console.log(API_OPTIONS);
-
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/top_rated?page=1",
       API_OPTIONS
